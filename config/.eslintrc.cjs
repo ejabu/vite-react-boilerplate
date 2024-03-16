595c0067fba0c0fd227e3ca0b441147809d9fbc2
ejabu <muhammadfahreza@gmail.com>
2024-03-17 00:31:00 +0700

    applying new formatting

--- config/.eslintrc.cjs
+++ config/.eslintrc.cjs
@@ -1,37 +1,34 @@
-module.exports ={
-    "env": {
-        "browser": true,
-        "es2022": true,
-        // "node": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-        // {
-        //     "files": ['**/*.cjs'],
-        //     "env": {
-        //         "node": true,
-        //     },
-        // },
-    ],
-    "ignorePatterns": [
-        "dist",
-        // ".eslintrc.cjs",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'react/react-in-jsx-scope': 0,
-        'react/no-deprecated': 0,
-    }
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    // "node": true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    // {
+    //   files: ['**/*.cjs'],
+    //   env: {
+    //     node: true,
+    //   },
+    // },
+  ],
+  ignorePatterns: [
+    'dist',
+    // ".eslintrc.cjs",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/no-deprecated': 0,
+  },
 };
