8d178ccd27d2d1b8ae434e960fb75a2fc27ef61a
ejabu <muhammadfahreza@gmail.com>
2024-03-17 02:41:33 +0700

    add react hooks warning

---
+++ src/components/ClickEffect.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const ClickHook = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('count', count);
+  }, []);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+export default ClickHook;
