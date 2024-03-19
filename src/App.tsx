0665d78407fd48f2b67cba7fa1f8a7babb7c665f
ejabu <muhammadfahreza@gmail.com>
2024-03-20 01:38:07 +0700

    update eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg';
+import viteLogo from './vite.svg';
 
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -21,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -33,6 +32,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
