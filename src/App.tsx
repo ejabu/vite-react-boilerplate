35eb6fce42588ae7c5b541162e16d80c86be0bfd
ejabu <muhammadfahreza@gmail.com>
2024-03-21 01:29:32 +0700

    setting react query as provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import UserList from './features/user-list/UserList';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <div>
-      <UserList />
+      <QueryClientProvider client={queryClient}>
+        <UserList />
+      </QueryClientProvider>
     </div>
   );
 };
