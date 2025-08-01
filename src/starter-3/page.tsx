66d9fc481740beb77cd44f517c57e2b755bb3b99
Reza <muhammadfahreza@gmail.com>
2025-08-01 09:47:33 +0400

    update

---
+++ src/starter-3/page.tsx
@@ -0,0 +1,12 @@
+import "react-pdf-highlighter/dist/style.css";
+import Starter3Component from "./widget";
+
+const Page = () => {
+	return (
+		<div>
+			<Starter3Component />
+		</div>
+	);
+};
+
+export default Page;
